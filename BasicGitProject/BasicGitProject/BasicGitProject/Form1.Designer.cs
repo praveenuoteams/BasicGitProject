0cd640577e7f828b1769d45c4506670b8c95d94f
praveenunoteam <121026777+praveenuoteams@users.noreply.github.com>
2026-02-07 13:07:38 +0530

    Second commit

---
+++ BasicGitProject/BasicGitProject/BasicGitProject/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace BasicGitProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(180, 198);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(140, 31);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "Hello, UnoTeam!";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(180, 315);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(140, 31);
+            textBox2.TabIndex = 1;
+            textBox2.Text = "Good afternoon!";
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(524, 450);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Git Sample";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+    }
+}
